40a634e5cbf64029edb5b2f2c84a7303379f126a
Debug-Sam <74404094+Debug-Sam@users.noreply.github.com>
2021-05-14 20:19:33 +0200

    Class Node gemaakt, ingevuld met atributen en een constructor. Ook een functie die de volgende node aanroept bij het krijgen van A of B. Alle nodes gedefinieerd en getest of de node verwijst naar de goede volgende node

--- src/fsm/Main.java
+++ src/fsm/Main.java
@@ -1,7 +1,25 @@
 package fsm;
 
+import fsm.domain.Node;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        Node s0 = new Node("S0");
+        Node s1 = new Node("S1");
+        Node s2 = new Node("S2");
+        Node s3 = new Node("S3");
+
+        s0.setNodeA(s2);
+        s0.setNodeB(s1);
+        s1.setNodeA(s1);
+        s1.setNodeB(s2);
+        s2.setNodeB(s3);
+        s3.setNodeA(s3);
+        s3.getNodeB(s0);
+
+        System.out.println(s0.volgendeNode('A'));
+
+
     }
 }
--- src/fsm/domain/Node.java
+++ src/fsm/domain/Node.java
@@ -1,4 +1,44 @@
 package fsm.domain;
 
 public class Node {
+
+    private String naam;
+    private Node nodeA;
+    private Node nodeB;
+
+    public Node(String naam) {
+        this.naam = naam;
+    }
+
+
+    public Node getNodeA() {
+        return nodeA;
+    }
+
+    public void setNodeA(Node nodeA) {
+        this.nodeA = nodeA;
+    }
+
+    public Node getNodeB(Node s0) {
+        return nodeB;
+    }
+
+    public void setNodeB(Node nodeB) {
+        this.nodeB = nodeB;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "naam='" + naam + '\'' +
+                '}';
+    }
+
+    public Node volgendeNode(char x) {
+        return switch (x) {
+            case 'A' -> nodeA;
+            case 'B' -> nodeB;
+            default -> null;
+        };
+    }
 }
